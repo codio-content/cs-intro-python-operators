29803d3c92a3886e8b853acf09f229c1563684d6
Patrick Ester <patricke@codio.com>
2019-12-12 22:26:59 +0000

    start lab 2 for recursion

---
+++ code/recursion/hilbert.py
@@ -0,0 +1,30 @@
+import turtle
+
+def hilbert2(dist, rule, angle, depth, t):
+    if depth > 0:
+        if rule == 1:
+            t.left(angle)
+            hilbert(dist, 2, angle, depth - 1, t)
+            t.forward(dist)
+            t.right(angle)
+            hilbert(dist, 1, angle, depth - 1, t)
+            t.forward(dist)
+            hilbert(dist, 1, angle, depth - 1, t)
+            t.right(angle)
+            t.forward(dist)
+            
+        if rule == 2:
+            t.right(angle)
+            hilbert(dist, 1, angle, depth - 1, t)
+            t.forward(dist)
+            t.left(angle)
+            hilbert(dist, 2, angle, depth - 1, t)
+            t.forward(dist)
+            hilbert(dist, 2, angle, depth - 1, t)
+            t.left(angle)
+            t.forward(dist)
+            hilbert(dist, 1, angle, depth - 1, t)
+            t.right(angle)
+        
+t = turtle.Turtle()
+hilbert(5, 1, 90, 5, t)
